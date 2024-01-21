85388f790b7a3cf3b4a25643a52bd742147392b0
Sebastián Castaño <sebas.cas96@hotmail.com>
2024-01-20 21:39:22 -0500

    fix: Skeleton folder moved to components folder.

--- src/app/items/loading.tsx
+++ src/app/items/loading.tsx
@@ -1,5 +1,5 @@
-import BreadcrumSkeleton from '@/components/organisms/_skeleton/breadcrum/breadcrum.skeleton';
-import ProductListSkeleton from '@/components/organisms/_skeleton/product-list/product-list.skeleton';
+import BreadcrumSkeleton from '@/components/_skeleton/breadcrum/breadcrum.skeleton';
+import ProductListSkeleton from '@/components/_skeleton/product-list/product-list.skeleton';
 import { EXTERNAL_API_LIMIT } from '@/constants/globals';
 
 export default function Loading() {
